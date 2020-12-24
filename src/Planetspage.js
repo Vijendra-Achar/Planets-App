f5680a0b01d2ccc71c266aa94053047720512da9
Vijendra-Achar <vijendra.1995@gmail.com>
2020-12-24 07:54:59 +0530

    Add and remove planets from Favs ✔

--- src/Planetspage.js
+++ src/Planetspage.js
@@ -3,11 +3,15 @@ import Planets from './Planets';
 function PlanetPage() {
   return (
     <div className='App myMargin'>
-      <h3 className='display-5'>Hello Planets App</h3>
+      <h3 className='display-5'>Hello, Welcome Planets App</h3>
 
       <br />
 
-      <h5 className='display-6'>🌍The List of All the planets are given below🌏</h5>
+      <h5 className='display-6'>The List of All the planets are given below</h5>
+
+      <br />
+
+      <p>Add the one's you like to the Favourite's List</p>
 
       <br />
 
--- src/YourFavPlanets.js
+++ src/YourFavPlanets.js
@@ -5,34 +5,60 @@ function YourFavPlanets() {
 
   useEffect(() => {
     getFavPlanets();
-  }, []);
+  }, [favPlanets]);
 
   const getFavPlanets = () => {
     let favs = localStorage.getItem('favs');
     setFavPlanets(JSON.parse(favs));
   };
 
-  return (
-    <div className='App myMargin'>
-      <h3>Your Fav Planets Page</h3>
+  const removeFromFavs = (ev) => {
+    let favs = JSON.parse(localStorage.getItem('favs'));
+    let value = ev.target.dataset.planet;
+    let arr = favs;
 
-      <br />
-      <h5 className='display-6'>🌍 The List of Your Favourite planets 🌏</h5>
-      <br />
-      <div className='myApp'>
-        {favPlanets.map((planet) => (
-          <ul key={planet} className='list-group'>
-            <li className='list-group-item d-flex justify-content-between align-items-center toTitle'>
-              {planet}
-              <span>
-                <button className='btn btn-danger'>Remove from Favs</button>
-              </span>
-            </li>
-          </ul>
-        ))}
+    for (let i = 0; i <= arr.length; i++) {
+      if (arr[i] === value) {
+        arr.splice(i, 1);
+        break;
+      }
+    }
+    localStorage.setItem('favs', JSON.stringify(arr));
+  };
+
+  if (favPlanets.length > 0) {
+    return (
+      <div className='App myMargin'>
+        <br />
+        <h5 className='display-6'>The List of Your Favourite planets</h5>
+        <br />
+
+        <div className='myApp'>
+          {favPlanets.map((planet) => (
+            <ul key={planet} className='list-group'>
+              <li className='list-group-item d-flex justify-content-between align-items-center toTitle'>
+                {planet}
+                <span>
+                  <button data-planet={planet} onClick={removeFromFavs} className='btn btn-danger'>
+                    Remove from Favs
+                  </button>
+                </span>
+              </li>
+            </ul>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className='App myMargin'>
+        <br />
+        <h5 className='display-6'>The List of Your Favourite planets</h5>
+        <br />
+        <p> You currently do not have any Favourite Planets. Please Add them </p>
+      </div>
+    );
+  }
 }
 
 export default YourFavPlanets;
